cd023ce6bc59bd618c18dfa8c475d84fc7cb97c7
vlion72 <valentin.lion@gmail.com>
2019-02-14 10:28:28 +0100

    dibidou

--- app/src/main/java/com/m1info/baki/bakitheclicker/LevelSelectActivity.java
+++ app/src/main/java/com/m1info/baki/bakitheclicker/LevelSelectActivity.java
@@ -1,7 +1,11 @@
 package com.m1info.baki.bakitheclicker;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 public class LevelSelectActivity extends AppCompatActivity {
 
@@ -9,5 +13,33 @@ public class LevelSelectActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_level_select);
+
+
+
+
+        Button btnBck = (Button)findViewById(R.id.buttonBack);
+        //ImageButton btn1 = (ImageButton)findViewById(R.id.imageButton1);
+
+
+
+        btnBck.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                startActivity(new Intent(LevelSelectActivity.this, MainMenuActivity.class));
+            }
+        });
+
+
+        /*btn1.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+
+                Intent intent = new Intent(LevelSelectActivity.this, FightActivity.class);
+                intent.putExtra("level", 1);
+                startActivity(intent);
+            }
+        });*/
+
+
+
     }
+
 }
